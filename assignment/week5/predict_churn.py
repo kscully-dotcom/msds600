7b1aa4de8618b1c8c8cd9d4d8a0147d446f469fe
kscully-dotcom <betz@my.ccsu.edu>
2021-09-27 23:46:46 -0500

    Finished 1st Version of Week 5 Assignment
    Go back and optimize more, work on python script, packaging, guis, etc.

--- assignment/week5/predict_churn.py
+++ assignment/week5/predict_churn.py
@@ -1,28 +1,26 @@
 import pandas as pd
 from pycaret.classification import predict_model, load_model
+class PredictChurn:
 
-def load_data(filepath):
-    """
-    Loads churn data into a DataFrame from a string filepath.
-    """
-    df = pd.read_csv(filepath, index_col='customerID')
-    return df
+    def load_data(filepath):
+        """
+        Loads churn data into a DataFrame from a string filepath.
+        """
+        df = pd.read_csv(filepath, index_col='customerID')
+        return df
 
+    def make_predictions(df):
+        """
+        Uses the pycaret best model to make predictions on data in the df dataframe.
+        """
+        model = load_model('gbc')
+        predictions = predict_model(model, data=df)
+        predictions.rename({'Label': 'of Churn', 'Score': 'Probability'}, axis=1, inplace=True)
+        predictions['of Churn'].replace({1: 'Churn', 0: 'No Churn'},
+                                                inplace=True)
+        return predictions[{'of Churn', 'Probability'}]
 
-def make_predictions(df):
-    """
-    Uses the pycaret best model to make predictions on data in the df dataframe.
-    """
-    model = load_model('cat')
-    predictions = predict_model(model, data=df)
-    predictions.rename({'Label': 'Churn_prediction'}, axis=1, inplace=True)
-    predictions['Churn_prediction'].replace({1: 'Churn', 0: 'No Churn'},
-                                            inplace=True)
-    return predictions['Churn_prediction']
-
-
-if __name__ == "__main__":
-    df = load_data('new_churn_data.csv')
-    predictions = make_predictions(df)
-    print('predictions:')
-    print(predictions)
+    if __name__ == "__main__":
+        df = load_data('new_churn_data.csv')
+        predictions = make_predictions(df)
+        print(predictions)
